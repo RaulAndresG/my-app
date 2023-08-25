d5a0335bf8f46a644990b15f8b9d17dac857841f
RaulAndresG <reaulgomes26@gmail.com>
2023-08-25 07:23:26 -0500

    feat

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import'./ExpenseItem.css';
-function ExpenseItem() {
+function ExpenseItem(props) {
     const expenseDate = new Date(2021,2,28);
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 294.67;
@@ -7,10 +7,16 @@ function ExpenseItem() {
 
     return (
     <div className='expense-item' >
-        <div> {expenseDate.toISOString()} </div>
+        <div> 
+        <div> {props.date.toLocleString()} </div>
+        <div>  </div>
+        <div>  </div>
+            
+            
+         </div>
         <div className='expense-item__description' >
-            <h2>{expenseTitle}</h2>
-            <div className='expense-item__price' >${expenseAmount} </div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price' >${props.amount} </div>
         </div>
         
     </div>
